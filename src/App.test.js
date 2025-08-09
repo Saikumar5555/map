37552a0f720b751c07b70cbe787398423dcd8dbc
mohan-gudey <mohan.intelliod@gmail.com>
2025-08-09 12:30:26 +0530

    Fix Jest CSS import issue for Leaflet

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders map heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Visakhapatnam/i); // or whatever is in your app
+  expect(heading).toBeInTheDocument();
 });
